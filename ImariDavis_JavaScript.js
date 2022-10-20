08da5cf1ea49084ac57e09a1e1d86a8d4899c6c7
IDD00 <67290544+IDD00@users.noreply.github.com>
2022-10-20 09:50:36 -0400

    Some form tests

---
+++ ImariDavis_JavaScript.js
@@ -0,0 +1,6 @@
+var userName;
+
+function confirmInput() {
+    userName = document.forms[0].userName.value;
+    alert("Hello " + userName + "! You will now be redirected to my (WIP) portfolio!");
+  }
